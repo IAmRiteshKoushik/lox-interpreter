ae2c2f2f6bf9a9a03b86e10541ca851fbe4b293d
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-11-15 21:22:04 +0530

    add: Setup Java SDK 17

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -7,64 +7,58 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 import java.util.StringTokenizer;
+
 
 public class Lox {
 
-  static boolean hadError = false;
-
   public static void main(String[] args) throws IOException {
-    if(args.length > 1){
-      System.out.println("Usage: jlox [script]"); 
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
       System.exit(64);
-    } else if (args.length == 1){
+    } else if (args.length == 1) {
       runFile(args[0]);
     } else {
       runPrompt();
     }
   }
-
+  static boolean hadError = false;
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
-
-    // Exit if error (with error code)
-    if(hadError) System.exit(65);
+    if (hadError) System.exit(65);
   }
-
-  // Interactive running of prompt
   private static void runPrompt() throws IOException {
     InputStreamReader input = new InputStreamReader(System.in);
     BufferedReader reader = new BufferedReader(input);
 
     for (;;) {
-      System.out.println("> ");
+      System.out.print("> ");
       String line = reader.readLine();
       if (line == null) break;
       run(line);
+    }
+    hadError = false;
+  }
 
-      hadError = false; // Resetting this error in interactive loop
-      // One prompt can have some error and fail to run but the program
-      // cannot fail completely. The next prompt should run normally
-      // for which the hadError flag has to be cleared.
-      
+  private static void run(String source) {
+    StringTokenizer tokens = new StringTokenizer(source);
+
+    // For now, just print the tokens.
+    while(tokens.hasMoreTokens()){
+      System.out.println(tokens.nextToken());
     }
   }
 
-  private static void run(String source){
-
-    for (Token token: tokens){
-      System.out.println(token);
-    }
-  }
-
-  // Error handling - should be able to handle different types of errors
-  static void error(int line, String message){
+  static void error(int line, String message) {
     report(line, "", message);
   }
-  // Design the report message for the error
-  private static void report(int line, String where, String message){
-    System.err.println("[line" + line + "] Error" + where + ": " + message);
+
+  private static void report(int line, String where, String message) {
+    System.err.println("[line " + line + "] Error" + where + ": " + message);
     hadError = true;
   }
+
+
 }
